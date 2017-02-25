02df481a7e7022fc88eb4abe215c9e1e3b2ca5a0
Angular CLI <angular-cli@angular.io>
2017-02-25 14:57:58 -0500

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { GildedRosePage } from './app.po';
+
+describe('gilded-rose App', () => {
+  let page: GildedRosePage;
+
+  beforeEach(() => {
+    page = new GildedRosePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
