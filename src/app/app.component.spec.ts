149beb5dcdf342357a0cdbacc4c7e885caf2e2b1
Vanya Dineva <dineva_vanya@yahoo.com>
2017-02-26 15:56:17 -0500

    Adding some unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,30 +3,49 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+
+    fixture = TestBed.createComponent(AppComponent);
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'Store Inventory!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Store Inventory');
-  }));
-
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Store Inventory');
+  }));
+
+  it('should update sellIn and quality for the Aged Brie according to specs', async(() => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+
+    let nameBefore = compiled.querySelector('table > tbody > tr:first-child td:nth-child(1)').textContent;
+    let sellInBefore = compiled.querySelector('table > tbody > tr:first-child td:nth-child(2)').textContent;
+    let qualityBefore = compiled.querySelector('table > tbody > tr:first-child td:nth-child(3)').textContent;
+
+    compiled.querySelector('button').click();
+    fixture.detectChanges();
+    let nameAfter = compiled.querySelector('table > tbody > tr:first-child td:nth-child(1)').textContent;
+    let sellInAfter = compiled.querySelector('table > tbody > tr:first-child td:nth-child(2)').textContent;
+    let qualityAfter = compiled.querySelector('table > tbody > tr:first-child td:nth-child(3)').textContent;
+
+    //the product name shuold not have changed after the button was clicked
+    expect(nameBefore).toEqual(nameAfter);
+    //for Aged Brie the sellIn should have decreased by 1 after the button is clicked once
+    expect(sellInBefore).toEqual((parseInt(sellInAfter) + 1).toString());
+    //for Aged Brie the quality should have increased by 1 after the button is clicked once
+    expect(qualityBefore).toEqual((qualityAfter - 1).toString());
   }));
 });
