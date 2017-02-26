aedd44f971fdeb0771130e1a33e4e4cfc36eaa83
Vanya Dineva <dineva_vanya@yahoo.com>
2017-02-26 11:26:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'app works!';
+  MIN = 0;
+  MAX = 50;
+
+  title = 'Store inventory';
+  items = ['Aged Brie', 'Sulfarus the Legendary Sword', 'Backstage Pass',  'Conjured Shield', 'Regular Shield'];
+  inventory = [];
+
+  constructor() {
+    for (let item of this.items) {
+      var sellIn = this.getRandomNumber(this.MIN, this.MAX);
+      var quantity = this.getRandomNumber(this.MIN, this.MAX);
+
+      this.inventory.push(new Item(item, sellIn, quantity));
+    }
+  }
+
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 }
---
+++ src/app/item.ts
@@ -0,0 +1,15 @@
+
+/*
+export class Item {
+  constructor(public name: string, public sellIn: number, public quality: number) {
+    name = name;
+    sellIn = sellIn;
+    quality = quality;
+  }
+}
+*/
+
+export class Item {
+  constructor(public name: string, public sellIn: number, public quality: number) {
+  }
+}
